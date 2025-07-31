ebb7391eb3c66a59348318415fcdfea3c4720725
PhiLongcode <nguyenphilongls2k4@gmail.com>
2025-07-31 17:26:12 +0700

    Set database password for connection

--- src/persistence/ConnectDB.java
+++ src/persistence/ConnectDB.java
@@ -7,10 +7,23 @@ import java.sql.SQLException;
 public class ConnectDB {
     private static final String URL = "jdbc:mysql://localhost:3306/students?useSSL=false&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "123456";
 
     public Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        ConnectDB connectDB = new ConnectDB();
+        try {
+            connectDB.getConnection();
+            System.out.println("Connected to database successfully");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
